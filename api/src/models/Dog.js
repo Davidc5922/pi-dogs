359a8fb9e0acde95bfb7985525eff2a336edb5c8
deathstroke459 <101474305+deathstroke459@users.noreply.github.com>
2022-07-20 00:21:32 -0500

    base de datos

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id:{
+    type: DataTypes.UUID,
+    defaultvalue:DataTypes.UUIDV4,
+    allowNull:false,
+    primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    altura:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Peso:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Años_de_vida:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdInOB:{
+      type:DataTypes.BOOLEAN,
+      defaultvalue:true,
+      allowNull:false
+    }
   });
 };
+/* ID *
+Nombre *
+Altura *
+Peso *
+Años de vida */
